a15c8fd7bf971f8572c6d476a2d0e3b1ef5637f3
Alec M <39228141+HeavenlyEntity@users.noreply.github.com>
2019-08-27 18:43:53 -0700

    E2E testing completed

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 // Unit Test Example
@@ -30,3 +31,21 @@ test('Should generate a valid text output', () => {
 })
 
 
+test('Should add user to DOM', async () => {
+    const browser = await puppeteer.launch({
+        headless: true, // False make UI visible
+        // slowMo: 80,
+        args: ['--window-size=1920,1080']
+    })
+    const page = await browser.newPage();
+    page.goto('file:///C:/Users/apmas/Documents/Github/js-testing-introduction/index.html'
+    );
+    await page.waitForNavigation();
+    await page.click('input#name');
+    await page.type('input#name', 'Jean');
+    await page.click('input#age');
+    await page.type('input#age', '33');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent)
+    expect(finalText).toBe('Jean (33 years old)');
+}, 10000);
